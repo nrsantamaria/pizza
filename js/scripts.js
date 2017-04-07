43189fa96131aa27fb640853de9c1201bbe11d02
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-07 11:10:42 -0700

    create a PizzaStore prototype to calculate price of pizza based on pizza size selected by user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 //Business Logic
-function PizzaStore (pizzaSizes, pizzaToppings) {
+function PizzaStore (pizzaSizes, pizzaToppings, currentOrder) {
   this.pizzaSizes = ["Large", "Medium", "Small"];
   this.pizzaToppings = ["Extra Cheese", "Pepperoni", "Sausage", "Anchovy", "Mushrooms", "Olives"]
+  this.currentOrder;
 };
 
-function Order (customerName, orderPizzaSize, orderPizzaToppings1, orderPizzaToppings2, orderPizzaToppings3, orderPizzaToppings4, orderPizzaToppings5, orderPizzaToppings6) {
+function Order (customerName, orderPizzaSize) {
   this.customerName = customerName;
   this.orderPizzaSize = orderPizzaSize;
-  this.orderPizzaToppings = [orderPizzaToppings1, orderPizzaToppings2, orderPizzaToppings3, orderPizzaToppings4, orderPizzaToppings5, orderPizzaToppings6];
-  this.orderPrice = 10;
+  this.orderPizzaToppings;
+  this.pizzaPrice = 10;
 };
 
-Order.prototype.orderToppingsSelected = function(){
-  $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-    var pizzaToppingsInput = $(this).val();
-  });
+// Order.prototype.orderToppingsSelected = function(){
+//   $("input:checkbox[name=pizza-toppings]:checked").each(function(){
+//     var pizzaToppingsInput = $("input:checkbox[name=pizza-toppings]:checked").val();
+//   });
+//   this.orderPizzaToppings = pizzaToppingsInput;
+// };
+
+PizzaStore.prototype.orderPrice = function (){
+  if (this.currentOrder.orderPizzaSize === this.pizzaSizes[0]){
+    this.currentOrder.pizzaPrice += 15;
+  } else if (this.currentOrder.orderPizzaSize === this.pizzaSizes[1]){
+    this.currentOrder.pizzaPrice += 10;
+  } else if (this.currentOrder.orderPizzaSize === this.pizzaSizes[2]){
+    this.currentOrder.pizzaPrice += 5;
+  }
 };
-
-
 
 //User Interface Logic
 $(document).ready(function(){
@@ -25,10 +35,22 @@ $(document).ready(function(){
     event.preventDefault();
     var nameInput = $("#name").val()
     var pizzaSizeInput = $(".pizza-size").val()
-    var newOrder = new Order(nameInput, pizzaSizeInput, pizzaToppingsInput);
-    var newPizzaStore = new PizzaStore();
-    console.log(newOrder.orderToppingsSelected);
+
+    var newOrder = new Order(nameInput, pizzaSizeInput);
+    var newPizzaStore = new PizzaStore(newOrder);
+    newPizzaStore.currentOrder = newOrder;
+
+    newPizzaStore.currentOrder.orderPizzaSize = pizzaSizeInput;
+    newPizzaStore.orderPrice();
+
     $("#customerName").append(newOrder.customerName);
-    $("#orderPizzaSize").append(newOrder.orderPizzaSize);      $("#orderPizzaToppings").append("<li>" + newOrder.orderToppingsSelected + "</li>");
+    $("#orderPizzaSize").append(newOrder.orderPizzaSize);
+
+    $("input:checkbox[name=pizza-toppings]:checked").each(function(){
+      var pizzaToppingsInput = $(this).val();
+      $("#orderPizzaToppings").append("<li>" + pizzaToppingsInput + "</li>");
+    });
+
+    $("#total-cost").append(newPizzaStore.currentOrder.pizzaPrice);
   });//form function close
 });//document ready function close
