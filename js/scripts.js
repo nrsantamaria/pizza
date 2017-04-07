0c50d6175cbcf5c9124a5dd884dce48874759421
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-07 12:02:13 -0700

    update output section in index.html. add font styles to styles.css. update scripts.js to include pizza toppings function.

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,11 @@ function PizzaStore (pizzaSizes, pizzaToppings, currentOrder) {
 function Order (customerName, orderPizzaSize) {
   this.customerName = customerName;
   this.orderPizzaSize = orderPizzaSize;
-  this.orderPizzaToppings;
+  this.orderPizzaToppings = [];
   this.pizzaPrice = 10;
 };
 
-// Order.prototype.orderToppingsSelected = function(){
-//   $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-//     var pizzaToppingsInput = $("input:checkbox[name=pizza-toppings]:checked").val();
-//   });
-//   this.orderPizzaToppings = pizzaToppingsInput;
-// };
-
-PizzaStore.prototype.orderPrice = function (){
+PizzaStore.prototype.orderPricePizzaSize = function (){
   if (this.currentOrder.orderPizzaSize === this.pizzaSizes[0]){
     this.currentOrder.pizzaPrice += 15;
   } else if (this.currentOrder.orderPizzaSize === this.pizzaSizes[1]){
@@ -27,21 +20,24 @@ PizzaStore.prototype.orderPrice = function (){
   } else if (this.currentOrder.orderPizzaSize === this.pizzaSizes[2]){
     this.currentOrder.pizzaPrice += 5;
   }
-  // if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[0]){
-  //   this.currentOrder.pizzaPrice += 1;
-  // } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[1] || this.currentOrder.orderPizzaToppings === this.pizzaToppings[2]){
-  //   this.currentOrder.pizzaPrice += 4;
-  // } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[3]){
-  //   this.currentOrder.pizzaPrice += 3;
-  // } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[4] || this.currentOrder.orderPizzaToppings === this.pizzaToppings[5]){
-  //   this.currentOrder.pizzaPrice += 2;
-  // }
+};
+
+PizzaStore.prototype.orderPricePizzaTopping = function (){
+    if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[0]){
+    this.currentOrder.pizzaPrice += 1;
+  } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[1] || this.currentOrder.orderPizzaToppings === this.pizzaToppings[2]){
+    this.currentOrder.pizzaPrice += 4;
+  } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[3]){
+    this.currentOrder.pizzaPrice += 3;
+  } else if (this.currentOrder.orderPizzaToppings === this.pizzaToppings[4] || this.currentOrder.orderPizzaToppings === this.pizzaToppings[5]){
+    this.currentOrder.pizzaPrice += 2;
+  }
 };
 
 Order.prototype.resetFields = function (){
   $("#name").val("");
   $(".pizza-size").val("");
-  $("input:checkbox[name=pizza-toppings]").prop( "checked", false );
+  $("input:checkbox[name=pizza-toppings]").prop("checked", false);
 };
 
 //User Interface Logic
@@ -53,18 +49,21 @@ $(document).ready(function(){
 
     var newOrder = new Order(nameInput, pizzaSizeInput);
     var newPizzaStore = new PizzaStore(newOrder);
-    newPizzaStore.currentOrder = newOrder;
 
+    newPizzaStore.currentOrder = newOrder;
     newPizzaStore.currentOrder.orderPizzaSize = pizzaSizeInput;
-    newPizzaStore.orderPrice();
 
     $("#customerName").append(newOrder.customerName);
     $("#orderPizzaSize").append(newOrder.orderPizzaSize);
 
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
       var pizzaToppingsInput = $(this).val();
+      newOrder.orderPizzaToppings.push(pizzaToppingsInput);
       $("#orderPizzaToppings").append("<li>" + pizzaToppingsInput + "</li>");
     });
+
+    newPizzaStore.orderPricePizzaSize();
+    newPizzaStore.orderPricePizzaTopping();
 
     $("#total-cost").append(newPizzaStore.currentOrder.pizzaPrice);
     newOrder.resetFields();
